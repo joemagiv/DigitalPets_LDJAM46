3ab29a47f44b0f83397deba97ecc41e6a173b33e
joemag_games <joemagiv@gmail.com>
2020-04-18 14:43:13 -0700

    Animations and animators added. Deprecated debug scipt. Next step is updating VirtualPet script to talk to sprite elements.

--- Assets/DigitalPetDebug.cs
+++ Assets/DigitalPetDebug.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DigitalPet : MonoBehaviour
+public class DigitalPetDebug : MonoBehaviour
 {
     //UI Text Elements
     public Text HealthText;
